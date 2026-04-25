cd9cf3e1258b8375ddbcc2310de3c9d77f4901f3
Jonathan S <jonathanimmanue007@gmail.com>
2026-04-24 19:24:46 -0700

    Fix: Implemented Instagram Deep-Spoof with official App IDs and Fetch metadata

--- backend/routes/downloader.js
+++ backend/routes/downloader.js
@@ -71,14 +71,21 @@ router.post('/info', async (req, res) => {
 
         // Platform-specific header adjustments
         if (platform === 'YouTube') {
-            options.addHeader[0] = 'referer:https://www.youtube.com/ ';
+            options.addHeader[0] = 'referer:https://www.youtube.com/';
         }
         if (platform === 'Instagram') {
-            options.addHeader[0] = 'referer:https://www.instagram.com/ ';
-            options.addHeader[1] = 'user-agent:Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1';
+            options.addHeader = [
+                'referer:https://www.instagram.com/',
+                'user-agent:Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+                'x-ig-app-id:936619743392459',
+                'x-asbd-id:129477',
+                'sec-fetch-dest:empty',
+                'sec-fetch-mode:cors',
+                'sec-fetch-site:same-origin'
+            ];
         }
         if (platform === 'Facebook') {
-            options.addHeader[0] = 'referer:https://www.facebook.com/ ';
+            options.addHeader[0] = 'referer:https://www.facebook.com/';
         }
 
         // Only apply player_client to YouTube
